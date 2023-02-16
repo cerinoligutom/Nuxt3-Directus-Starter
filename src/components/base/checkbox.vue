0ac8befbf2cd7c01098f40306936dc0e566197e9
Cerino Ligutom III <zeferinix@gmail.com>
2023-02-17 00:03:48 +0800

    feat: implement checkbox

---
+++ src/components/base/checkbox.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="form-control">
+    <label class="cursor-pointer label">
+      <input v-model="modelValue" type="checkbox" class="mr-2 checkbox checkbox-primary" />
+      <span class="label-text">{{ label }}</span>
+    </label>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  inheritAttrs: false,
+};
+</script>
+
+<script lang="ts" setup>
+import { useVModel } from '@vueuse/core';
+
+interface IProps {
+  label?: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  modelValue: boolean;
+}
+const props = defineProps<IProps>();
+
+interface IEmits {
+  (e: 'update:modelValue', modelValue: boolean): void;
+}
+const emit = defineEmits<IEmits>();
+
+const modelValue = useVModel(props, 'modelValue', emit);
+</script>
